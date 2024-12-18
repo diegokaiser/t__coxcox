ed7e5c8b36dcb9da713cf55bc1e21546459ea66f
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2024-12-18 15:30:39 +0100

    enviando

--- components/atoms/index.tsx
+++ components/atoms/index.tsx
@@ -1,5 +1,2 @@
 export { default as Button } from './Button';
-export { default as CalendarSelect } from './CalendarSelect';
-export { default as HourSelect } from './HourSelect';
 export { default as Loading } from './Loading';
-export { default as PeopleSelect } from './PeopleSelect';
