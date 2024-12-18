d3b7c6b9cbbdd4514cb2091f1d06f0a7f2398614
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2024-12-18 16:03:42 +0100

    enviando

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
+import MillionLint from "@million/lint";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, dns: false, child_process: false, tls: false }
     return config
   }
 };
 
-export default nextConfig;
+export default MillionLint.next({
+  enabled: true,
+  rsc: true
+})(nextConfig);
